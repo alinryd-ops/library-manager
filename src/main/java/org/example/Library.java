d8f0e58efbb4cd323624544920b43b44653da0b6
alinryd-ops <alinryd@gmail.com>
2026-09-15 13:36:04 +0200

    git add src/main/java/org/example/Book.java git commit -m "Added borrowBook method to Library"

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -49,6 +49,46 @@ public class Library {
         IO.println("Member id number is " + newMember.getId());
     }
 
+    public void borrowBook(String isbn, int memberId) {
+        int bookIndex = -1;
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].isbn().equalsIgnoreCase(isbn)) {
+                bookIndex = i;
+                break;
+            }
+        }
 
+        Member foundMember = null;
+        for (int i = 0; i < memberCount; i++) {
+            if (members[i].getId() == memberId) {
+                foundMember = members[i];
+                break;
+            }
+        }
+
+        if (bookIndex == -1) {
+            IO.println("Book not found");
+            return;
+        }
+
+        if (foundMember == null) {
+            IO.println("Member not found");
+            return;
+        }
+
+        if (borrowedby[bookIndex] != -1) {
+            IO.println("This book is already borrowed");
+            return;
+        }
+
+        if (!foundMember.canBorrow()) {
+            IO.println("This member has reached the maximum number of loans");
+            return;
+        }
+
+        borrowedby[bookIndex] = memberId;
+        foundMember.increaseLoans();
+        IO.println("Book borrowed successfully");
+    }
 
 }
