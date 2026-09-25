b5aa9409ee350f970b0aa220ea9e90a5b6859b65
alinryd-ops <alinryd@gmail.com>
2026-09-25 14:11:38 +0200

    git add . git commit -m "Make member id and name final"

--- src/main/java/org/example/Member.java
+++ src/main/java/org/example/Member.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Member {
 
-    private int id;
-    private String name;
+    private final int id;
+    private final String name;
     private int activeLoans;
 
     public Member(int id, String name)  {
